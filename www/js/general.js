8a8c40414d20bcfae228f7cb2479b6c92428bff9
Marcus Fischer <xmarcus89@yahoo.de>
2013-11-16 14:18:02 +0100

    Erweitert und beendet in Funktionalität. (Dynamischer Text mit Klick).
    Platformen selektiert.
    Version 3.1.0 geändert.

--- www/js/general.js
+++ www/js/general.js
@@ -11,6 +11,12 @@ var i18n = {
             callback: function() {
                 document.getElementById('hello').innerHTML = jQuery.i18n.prop('msg_hello');
                 document.getElementById('world').innerHTML = jQuery.i18n.prop('msg_world');
+                document.getElementById('label_lang').innerHTML = $.i18n.prop('m_label_lang');
+                document.getElementById('lang_browser').innerHTML = $.i18n.prop('m_lang_browser');
+                document.getElementById('lang_en').innerHTML = $.i18n.prop('m_lang_en');
+                document.getElementById('lang_de').innerHTML = $.i18n.prop('m_lang_de');
+                document.getElementById('lang_es').innerHTML = $.i18n.prop('m_lang_es');
+                document.getElementById('button').innerHTML = $.i18n.prop('m_button');
             }
         });
     },
@@ -21,5 +27,18 @@ var i18n = {
             selectionLang = jQuery.i18n.browserLang();
         }
         this.updateTexts(selectionLang);
+    },
+
+    createText: function() {
+        var nodes = document.getElementById('createdTextDiv').childNodes;
+        var count;
+        if(nodes == undefined) {
+            count = 1;
+        } else {
+            count = nodes.length + 1;
+        }
+        var text = document.createElement('span');
+        text.innerHTML = $.i18n.prop('m_automatic_text') + ' ' + count + '<br>';
+        document.getElementById('createdTextDiv').appendChild(text);
     }
 };
